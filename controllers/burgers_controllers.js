6b6c0416ba9b1c9e9b17f05b31fdbe37b841919a
Angelo Capozzi <acap613@gmail.com>
2019-12-21 13:35:02 -0500

    added handlebars, stylesheets, working on getting logic to work

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -17,21 +17,21 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burger", function(req, res) {
-  cat.create([
-    "name", "sleepy"
+  burger.create([
+    "burger_name", "devoured"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.delete("/api/cats/:id", function(req,res){
+router.delete("/api/burgers/:id", function(req,res){
   var condition = "id = " + req.params.id;
 
 
-  cat.delete(condition, function(result){
+  burger.delete(condition, function(result){
     if (result.affectedRows == 0) {
       return res.status(404).end();
     } else {
@@ -40,13 +40,13 @@ router.delete("/api/cats/:id", function(req,res){
   })
 })
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
+  burger.update({
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
